85802ef6f60232335f87dccc25a2366443f5bb2d
Marlo <marlo@mono-gusa.de>
2018-10-24 12:23:59 +0200

    first project structure

---
+++ src/Decimal.cpp
@@ -0,0 +1,18 @@
+/*
+ * Decimal.cpp
+ *
+ *  Created on: 24.10.2018
+ *      Author: marlo
+ */
+
+#include "Decimal.h"
+
+Decimal::Decimal() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Decimal::~Decimal() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Decimal.h
@@ -0,0 +1,17 @@
+/*
+ * Decimal.h
+ *
+ *  Created on: 24.10.2018
+ *      Author: marlo
+ */
+
+#ifndef DECIMAL_H_
+#define DECIMAL_H_
+
+class Decimal {
+public:
+	Decimal();
+	~Decimal();
+};
+
+#endif /* DECIMAL_H_ */
