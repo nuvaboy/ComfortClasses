df519042e9adfbea14fd89f8d9b0049950199f7a
Marlo <marlo@mono-gusa.de>
2018-10-28 02:27:13 +0200

    Addition funktioniert (grob getestet)
    Tests
    - Verschiebungen der Nachkommastelle (Variationen)
    - Trailing Zeros

--- src/CCDecimal.cpp
+++ src/CCDecimal.cpp
@@ -7,17 +7,171 @@
 
 #include "CCDecimal.h"
 
+using namespace std;
+
+unsigned int CCDecimal::defaultPrecision = 6;
+
 CCDecimal::CCDecimal() {
-	// TODO Auto-generated constructor stub
-	/**
-	 * Das ist Jans Beitrag zur Gesellschaft.
-	 */
+	pPrecision = &CCDecimal::defaultPrecision;
+
+	//initialize all digits to zero
+	for (int i = 0; i <= MAX; i++) {
+		digit[i] = 0;
+	}
+	used = 0;
+	shift = 0;
 }
 
 CCDecimal::~CCDecimal() {
-	// TODO Auto-generated destructor stub
+
+}
+
+CCDecimal CCDecimal::operator +(const CCDecimal& op2) const {
+	CCDecimal result;
+	add(&result, op2);
+
+	return result;
+}
+
+bool CCDecimal::operator ==(const CCDecimal& op2) const {
+	if (used != op2.used || shift != op2.shift) {
+		cout << "false, shift or used" << endl;
+		return false;
+	}
+
+	for (unsigned int i = 0; i < used; i++) {
+		if (digit[i] != op2.digit[i]) {
+			cout << "Index: " << i << " op1: " << digit[i]  << " op2: " <<  op2.digit[i] << endl;
+			return false;
+		}
+	}
+	return true;
+}
+
+void CCDecimal::add(CCDecimal* result, const CCDecimal& op2) const {
+	//temporary values
+	int shift_delta = shift - op2.shift; //distance between decimal points of both operands
+	int resultOffset1 = 0; //offset for adding operand 1 to result, zero if shift of operand 1 is less than that of operand 2
+	int resultOffset2 = 0; //offset for adding operand 2 to result, zero if shift of operand 2 is less than that of operand 1
+	int opOffset1 = 0; //offset for addressing operand 1
+	int opOffset2 = 0; //offset for addressing operand 2
+	int shift_min = op2.shift; //max precision (operand 1, operand 2)
+	int size = 0; //coalesced size to hold result of the addition (without overflow and removal of trailing zeros)
+
 	/*
-	 * dfhalöshfashdfkj dkfjdsfölasdkfjadslfaösld
+	 * 1.) calculate highest precision (lowest exponent) : "shift_min"
+	 * 2.) calculate distance between the decimal points of both operators : "shift_delta"
+	 * 3.) apply distance as offset for the operator with the highest exponent : "sIndex1" or "sIndex2"
+	 * 4.) calculate coalesced size to hold the result of the addition
+	 *     (without regard to a possible overflow or the removal of trailing zeros)
 	 */
+	if (shift_delta < 0) { //operand 1 has highest precision (lowest exponent)
+
+		shift_min = shift; //save highest precision
+		shift_delta = -shift_delta; //save distance between decimal points (operand 1, operand 2)
+		resultOffset2 = shift_delta; //apply distance as offset for operand 2
+		size = max(used, shift_delta + op2.used); //calculate coalesced size
+
+	} else { //operand 2 has highest (or equal) precision (lowest exponent)
+		resultOffset1 = shift_delta; //apply distance as offset for operand 1
+		size = max(op2.used, shift_delta + used); //calculate coalesced size
+
+	}
+	cout << "calculated size: " << size;
+
+	//calculate amount of expendable digits
+	int digToSpend = -precision - shift_min;
+	cout << "   to spend: " << digToSpend;
+
+	//calculate amount of digits needed to be cut to meet size requirements
+	int digToCut = size - MAX; //size - 31 = digits to cut
+	cout << "   to cut: " << digToCut;
+
+	//requiered to cut
+	if (digToCut > 0) {
+		size = MAX;
+
+		if (digToSpend < digToCut) { //overflow
+			cout << "   overflow ";
+
+		} else { //cut
+			shift_min += digToCut;
+			if (resultOffset1 == 0) { //operand 1 has highest precision
+				opOffset1 = digToCut;
+				opOffset2 = (digToCut > resultOffset2) ? (digToCut - resultOffset2) : 0;
+				resultOffset2 = (resultOffset2 > digToCut) ? (resultOffset2 - digToCut) : 0;
+
+			} else { //operand 2 has highest precision
+				opOffset2 = digToCut;
+				opOffset1 = (digToCut > resultOffset1) ? (digToCut - resultOffset1) : 0;
+				resultOffset1 = (resultOffset1 > digToCut) ? (resultOffset1 - digToCut) : 0;
+			}
+		}
+	}
+
+	cout << "   shdelta: " << shift_delta;
+	cout << "   index1: " << resultOffset1;
+	cout << "   index2: " << resultOffset2;
+
+	//add operand 1 to the result
+	for (unsigned int i = resultOffset1; i < MAX && (i - resultOffset1 + opOffset1 < used); i++) {
+		result->digit[i] = digit[i - resultOffset1 + opOffset1];
+	}
+
+	//add operand 2 to the result
+	for (unsigned int i = resultOffset2; i < MAX && (i - resultOffset2 + opOffset2 < op2.used);
+			i++) {
+		result->digit[i] += op2.digit[i - resultOffset2 + opOffset2];
+	}
+
+	//forward carries
+	for (int i = 0; i < size; i++) {
+		if (result->digit[i] >= 10) {
+			result->digit[i] -= 10;
+			result->digit[i + 1]++;
+		}
+	}
+
+	//end/////////////////////
+	result->used = size;
+	result->shift = shift_min;
+	//////////////////////////
+
+	//remove trailing zeros
+	if (result->digit[0] == 0) {
+
+		int lastZero = 1; //TODO non zero
+		while (result->digit[lastZero] == 0 && lastZero < MAX) {
+			lastZero++;
+		}
+		for (int i = lastZero; i <= MAX; i++) {
+			result->digit[i - lastZero] = result->digit[i];
+		}
+		result->used -= (lastZero ); //- 1//overflow digit now visible, hence used = used - lastZero_+ 1_
+		result->shift += lastZero;
+		result->digit[MAX] = 0;
+	}
+
+	//TODO remove leading zeros in case of subtraction
+
+	if (result->digit[MAX] > 0) {
+
+		//overflow
+		throw std::overflow_error(
+				"Result is too large to store in Decimal. Keep values in range or reduce precision!");
+	}
+
+	cout << "   r: ";
+	//result->print();
+
+	cout << endl;
+}
+
+void CCDecimal::setDefaultPrecision(unsigned int precision) {
+	CCDecimal::defaultPrecision = precision;
+}
+
+unsigned int CCDecimal::getDefaultPrecision() {
+	return CCDecimal::defaultPrecision;
 }
 
--- src/CCDecimal.h
+++ src/CCDecimal.h
@@ -8,10 +8,135 @@
 #ifndef CCDECIMAL_H_
 #define CCDECIMAL_H_
 
+#define MAX 31
+
+
+#include <stdint.h>
+#include <iostream>
+#include <cstdarg>
+#include <string>
+
+using namespace std;
+
 class CCDecimal {
+private:
+
+	int8_t digit[MAX + 1];
+	unsigned int used = 0;
+	int shift = 0;
+	int precision = 2;
+
+	unsigned int* pPrecision;
+	static unsigned int defaultPrecision;
+
+	void add(CCDecimal* result, const CCDecimal&) const;
+
 public:
+
 	CCDecimal();
-	~CCDecimal();
+	virtual ~CCDecimal();
+
+	CCDecimal operator +(const CCDecimal&) const;
+	bool operator ==(const CCDecimal&) const;
+
+	static void setDefaultPrecision(unsigned int);
+	static unsigned int getDefaultPrecision();
+
+	void setDigit(unsigned int pos, int8_t value) {
+		digit[pos] = value;
+		if (pos >= used && value != 0) {
+			used = pos + 1;
+		}
+	}
+	void setDigits(int count, ...) {
+		//cout << count << endl;
+
+		va_list arguments;             // A place to store the list of arguments
+		va_start(arguments, count); // Initializing arguments to store all values after count
+		for (int i = count - 1; i >= 0; i--) {
+			int val = (int) (va_arg(arguments, int));
+			setDigit(i, val);
+			//cout << va_arg(arguments, int) << endl;
+		}
+		// Adds the next value in argument list to sum.
+		va_end(arguments);
+
+	}
+	void setShift(int shift) {
+		this->shift = shift;
+	}
+	CCDecimal(int count, ...) {
+		va_list arguments;             // A place to store the list of arguments
+		va_start(arguments, count); // Initializing arguments to store all values after count
+		for (int i = count - 1; i >= 0; i--) {
+			int val = (int) (va_arg(arguments, int));
+			setDigit(i, val);
+			//cout << va_arg(arguments, int) << endl;
+		}
+		// Adds the next value in argument list to sum.
+		va_end(arguments);
+	}
+
+	void print() const {
+			for (int i = used - 1; i >= 0; i--) {
+
+				cout << (int) digit[i];
+				if (i > 0 && i == -shift) {
+					cout << '.';
+				}
+			}
+			cout << 'E';
+			cout << shift;
+		}
+
+	CCDecimal operator =(const string& str) {
+
+		shift = 0;
+		int lz = 0;
+		bool hasDp = false;
+
+		unsigned int pos = 0;
+
+		int i = str.length() -1;
+		while (str[i] == '0') {
+			i--;
+		}
+		lz = str.length() - i -1;
+
+		for (; i >= 0; i--) {
+
+			if (pos >= MAX) {
+				throw std::exception();
+			}
+
+			char c = str[i];
+
+			if (c >= '0' && c <= '9') {
+				digit[pos] = c - 48;
+				pos++;
+				cout << digit[pos-1] << endl;
+				//print();
+			} else if (!hasDp && c == '.') {
+				hasDp = true;
+				shift = -pos;
+			} else {
+				cout << "some failure: " << i << " " << endl;
+				throw std::exception();
+			}
+		}
+
+		if (hasDp == false) {
+			shift = lz;
+		}
+		used = pos;
+		cout << "used: " << used  << endl;
+		cout << "shift: " <<  shift<< endl;
+
+		return *this;
+	}
+
+
+
 };
 
 #endif /* CCDECIMAL_H_ */
