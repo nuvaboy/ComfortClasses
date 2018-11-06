f3bd9c04a49b259ea0f404a2a5a6b8f1c4d357ab
Jan Alexander Stiehl <jan.alexander.stiehl@stud.hs-emden-leer.de>
2018-11-06 04:19:28 +0100

    test for behavior of removal while iterating

--- src/ComfortClasses.cpp
+++ src/ComfortClasses.cpp
@@ -6,9 +6,48 @@
  */
 
 #include <iostream>
+#include <string>
 
-int main(){
-	std::cout << "**Hello World!!!!**" << std::endl;
+int main() {
+	std::string str;
+
+	for (char c = '0'; c <= '9'; c++) {
+		str.push_back(c);
+	}
+
+	std::cout << str << std::endl;
+	for (auto rit = str.rbegin(); rit != str.rend(); rit++) {
+		std::cout << *rit;
+		if (*rit == '5') {
+			std::cout << " removing " << rit.base()[-1] << " ";
+			str.erase(rit.base()-1);
+		}
+		std::cout << *rit << std::endl;
+		
+	}
+
+	std::cout << std::endl << str << std::endl;
+
+	str.clear();
+
+	for (char c = '0'; c <= '9'; c++) {
+		str.push_back(c);
+	}
+
+	std::cout << str << std::endl;
+	for (auto it = str.begin(); it != str.end(); it++) {
+		std::cout << *it;
+		if (*it == '5') {
+			std::cout << " removing " << *it << " ";
+			str.erase(it);
+		}
+		std::cout << *it << std::endl;
+
+	}
+
+	std::cout << std::endl << str << std::endl;
+
+	std::cin >> str;
 }
 
 
