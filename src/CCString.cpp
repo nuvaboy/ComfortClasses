640158d11a79598b78c15e4e0d4b7f45266445a0
Jan Alexander Stiehl <jan.alexander.stiehl@stud.hs-emden-leer.de>
2019-01-21 10:30:18 +0100

    added string class frame

---
+++ src/CCString.cpp
@@ -0,0 +1,18 @@
+/*
+ * CCString.cpp
+ *
+ *  Created on: 21.01.2019
+ *      Author: jan
+ */
+
+#include "CCString.h"
+
+CCString::CCString() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CCString::~CCString() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/CCString.h
@@ -0,0 +1,17 @@
+/*
+ * CCString.h
+ *
+ *  Created on: 21.01.2019
+ *      Author: jan
+ */
+
+#ifndef CCSTRING_H_
+#define CCSTRING_H_
+
+class CCString {
+public:
+	CCString();
+	virtual ~CCString();
+};
+
+#endif /* CCSTRING_H_ */
