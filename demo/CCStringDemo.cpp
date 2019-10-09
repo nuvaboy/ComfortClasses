4233c1a9fa12dfa28628f5b795f51d9bb2e96e30
Jan Alexander Stiehl <jasd02@live.de>
2019-10-09 16:10:27 +0200

    HelloWorld in demo

--- demo/CCStringDemo.cpp
+++ demo/CCStringDemo.cpp
@@ -1,1 +1,8 @@
-#include "../src/CString.h"
+#include "../src/CCString.h"
+#include <iostream>
+
+int main(int n, char** args){
+    CCString test = "Hello world!\n";
+    std::cout << test;
+    return 0;
+}
