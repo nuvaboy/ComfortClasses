6fc59291dceb3dfe4ef3b1c446695c11b4e07a43
Jan Alexander Stiehl <jasd02@live.de>
2019-10-24 00:05:06 +0200

    CCStringDemo++ :
    - completed manipulation demo
    - minor tweaks to in-code documentation
    - reordering of partial demos

--- demo/CCStringDemo.cpp
+++ demo/CCStringDemo.cpp
@@ -10,38 +10,94 @@ using std::endl;
 using std::vector;
 
 /**
+ * Demonstrates the different manipulation functions CCString offers.
+ */
+void manipDemo()
+{
+    /**
+     * supported operations:    demonstrated:
+     * 
+     * - length()
+     * - at(),[]
+     * - append(),+=,<<,+       X
+     * - replaceAt()            X
+     * - insert()               X
+     * - erase()                X
+     * - trim()                 X
+     * - subString()            X
+     * - find()                 X
+     * - findLast()             X
+     */
+
+    // test string, with whitespace characters
+    CCString msg1 = " \n  This statement is false. \t   \n ";
+    // removes whitespace characters
+    msg1.trim();
+
+    // find the "false"
+    size_t falsePosition = msg1.findLast("is") + 3;
+    // and, on a copy, replace it with _mod1"
+    CCString msg1_mod1(msg1);
+    msg1_mod1.erase(falsePosition, 5).insert(falsePosition, "true");
+
+    // on a copy, replace "statement" with "equation "
+    // NOTE that replaceAt() overwrites entire portions of strings only
+    CCString msg1_mod2(msg1_mod1);
+    msg1_mod2.replaceAt(msg1_mod2.find("is") + 3, "equation ");
+
+    // another useless message, created by cutting out "is true.", then attaching "True " to the front:
+    CCString msg2 = msg1_mod1.subString(msg1_mod1.findLast("is"), CCString::npos).insert(0, "True ");
+
+    /**
+     * stitch everything together (using different ways to the same effect):
+     */
+    // using + operator (first element in chain should be guaranteed to be of CCString)
+    CCString msg_all = CCString("msg1:      ") + msg1 + "\n";
+    // using append() method
+    msg_all.append("msg1_mod1: ").append(msg1_mod1).append("\n");
+    // using += operator (equiv to append())
+    msg_all += "msg1_mod2: ";
+    msg_all += msg1_mod2;
+    msg_all += "\n";
+    // using << operator (equiv to append(); peferred method, deliberately 
+    // made to look like stream operator: working most consistently when chained)
+    msg_all << "msg2:      " << msg2 << "\n";
+
+    // output to console: (alternative method)
+    puts((const char*)msg_all);
+}
+
+/**
  * Demonstrates the different types CCString can accept as inputs using its constructors;
  * for others, conversion operators are neccessary.
  */
 void typesDemo()
 {
     /**
-     * Supported types:
-     * std::string
-     * const char*
-     * char
+     * supported types:         demonstrated:
      * 
-     * bool
+     * - std::string          
+     * - const char*            (implicitly)
+     * - char                   (implicitly)
      * 
-     * (u)int_(8,16,32,64)
-     * float
-     * (long) double
-     * CCDecimal
+     * - bool                   X
+     * 
+     * - (u)int_(8,16,32,64)    partially
+     * - float, (long) double   partially
+     * - CCDecimal              X
      */
 
     // Variables of several example types:
     int64_t negOne = -1;
     double aTenth = 0.1;
     CCDecimal accurateTenth = "0.1";
-    bool trueValue = true;
-    bool falseValue = false;
 
     CCString builtInTypes;
-    builtInTypes << "Here, we have...:\n "
-                 << "  an intege:   " << negOne << "\n"
-                 << "  a double:    " << aTenth << "\n"
-                 << "  a CCDecimal: " << accurateTenth << "\n"
-                 << "  and the boolean values, " << trueValue << " and " << falseValue << "\n";
+    builtInTypes << "Here, we have...:\n"
+                 << "- an integer   " << negOne << "\n"
+                 << "- a double:    " << aTenth << "\n"
+                 << "- a CCDecimal: " << accurateTenth << "\n"
+                 << "- and the boolean values, " << true << " and " << false << "\n";
 
     struct demoRational
     {
@@ -61,34 +117,23 @@ void typesDemo()
 }
 
 /**
- * Demonstrates the different manipulation functions CCString offers.
- */
-void manipDemo()
-{
-    /**
-     * supported operations:
-     * - length()
-     * - at(),[]
-     * - append(),+=,<<,+
-     * - replaceAt()
-     * - insert()
-     * - erase()
-     * - trim()
-     * - subString()
-     * - find()
-     * - findLast()
-     */
-
-
-}
-
-/**
  * Shows ways of disecting strings using the built-in regex functions,
  * using a URL as an example.
  */
 void regexDemo()
 {
-    /* 
+    /**
+     * supported operations:    demonstrated:
+     * 
+     * isMatch                  X
+     * containsMatch            X
+     * getMatch                 X
+     * replaceFirst             X
+     * replaceAll               explained
+     * splitBegin(),splitEnd()  X
+     */
+
+    /**
      * We start with a string containing a URI:
      */
     CCString resource = "http://192.168.1.2:8080/index.html";
@@ -198,13 +243,14 @@ void regexDemo()
 
 int main()
 {
-    cout << "\n---------------regexDemo: \n";
-    regexDemo();
-
     cout << "\n---------------manipDemo: \n";
     manipDemo();
 
     cout << "\n---------------typesDemo: \n";
     typesDemo();
+
+    cout << "\n---------------regexDemo: \n";
+    regexDemo();
+
     return 0;
 }
